bfb5aa10d6e211d680bf9b95698693b990629c87
luciendjob <luciendjob50@gmail.com>
2022-11-22 06:25:58 +0100

    creation du dossier des test d'integration de  l'application

---
+++ test-camsim/test.integration.annonce.js
@@ -0,0 +1,31 @@
+const assert = require('assert')
+const J = require('../CAMSIM STATIC DAJA JSON/encheres.json')
+const app = require('../app')
+
+
+const JO = null
+describe('BasicTest' , function(){
+
+    describe('api' , function() {
+        it('should not equal at null' , function(){
+            // const d = {nom : 'djob'}
+            // assert.notEqual(d , null )
+            const bcrypt = require('bcrypt')
+            console.log(bcrypt);
+        }  
+           
+        )// end it
+    })
+})
+
+
+describe('secondTest' , function(){
+
+    describe('other' , function(){
+        it('Should equal at undefined' , function(){
+            let a = 3;
+            assert.equal(a , undefined)
+        })
+    })
+
+})
